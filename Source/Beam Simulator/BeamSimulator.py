835ebb395d43c5974adf5e798c6ece75b9a9f820
(Jack) Kuei Hung Zhang <kh4zhang@uwaterloo.ca>
2022-11-15 22:36:46 -0500

    Edited BeamSimulator.py

--- Source/Beam Simulator/BeamSimulator.py
+++ Source/Beam Simulator/BeamSimulator.py
@@ -37,12 +37,15 @@ def RelativisticBeam(V: int, LM: int, RM: int):
     :return: np.array
     '''
 
+    # Declare a constant for the speed of light
+    CONST_c = 299792458
+
     # Define charge q_e and rest mass m_e of an electron
     q_e = 1.60217663e-19
     m_e = 9.1093837e-31
 
     # Find speed v_e of electron at the start of its trajectory
-    v_e = np.sqrt(2*(V*q_e)/m_e)
+    v_e = c*np.sqrt(1-(1/(1+V*q_e/(m_e*CONST_c**2)))**2)
 
     # Define initial velocity vector of electron. By convention, we will set the forward axis of the electron gun
     # assembly as the y-axis, with the direction of the right magnet being given by positive x-axis. By the right hand
@@ -50,3 +53,5 @@ def RelativisticBeam(V: int, LM: int, RM: int):
     vel_0 = np.array([0,v_e,0])
 
     # [code for defining magnetic field]
+
+if __name__ == '__main__':

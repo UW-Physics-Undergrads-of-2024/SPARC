faf95ac9c12bec60b5f6e66602003fcc08244a53
= <=>
2022-11-20 22:52:17 -0500

    Added function <WIP> to add curve for electron beam trajectory

--- Source/GUI.py
+++ Source/GUI.py
@@ -1,6 +1,6 @@
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qtg
-import vispy.scene as vp
+import vispy.scene as vs
 import numpy as np
 import vispy.visuals as vv
 
@@ -34,13 +34,13 @@ class MainWindow(qt.QMainWindow):
         self.setCentralWidget(self.centralWidget)
 
         # *********** Set Vispy Canvas ********************************
-        self.canvas = vp.SceneCanvas(title="Simulated SPARC", keys="interactive", bgcolor=(0.309, 0.345, 0.454, 1.0))
+        self.canvas = vs.SceneCanvas(title="Simulated SPARC", keys="interactive", bgcolor=(0.309, 0.345, 0.454, 1.0))
         self.view = self.canvas.central_widget.add_view()
         self.view.camera = "turntable"
         self.canvasLayout.layout().addWidget(self.canvas.native)
 
         # Add XYZ
-        axis = vp.visuals.XYZAxis(parent=self.view.scene)
+        axis = vs.visuals.XYZAxis(parent=self.view.scene)
         stransform = vv.transforms.STTransform(translate=(-5, -5), scale=(1, 1, 1, 1))
         affine = stransform.as_matrix()
         axis.transform = affine
@@ -52,13 +52,13 @@ class MainWindow(qt.QMainWindow):
 
         # Add board that all components will be mounted on
         boardMesh = trimesh.load(r"Mesh/SPARC_board.stl")
-        board = vp.visuals.Mesh(vertices=boardMesh.vertices, faces=boardMesh.faces, color=(0.8,0.4,0,1))
+        board = vs.visuals.Mesh(vertices=boardMesh.vertices, faces=boardMesh.faces, color=(0.8,0.4,0,1))
         board.attach(vv.filters.mesh.ShadingFilter(shading="smooth", ))
         self.view.add(board)
 
         # Add vacuum tube assembly to view
         vacuumMesh = trimesh.load(r"Mesh/SPARC_vacuum.stl")
-        vacuum = vp.visuals.Mesh(vertices=vacuumMesh.vertices, faces=vacuumMesh.faces, color='blue')
+        vacuum = vs.visuals.Mesh(vertices=vacuumMesh.vertices, faces=vacuumMesh.faces, color='blue')
         vacuum.attach(vv.filters.Alpha(0.2)) # makes mesh semi-transparent
         vacuum.attach(vv.filters.mesh.ShadingFilter(shading="smooth"))
         self.view.add(vacuum)
@@ -67,20 +67,20 @@ class MainWindow(qt.QMainWindow):
 
         # Add vacuum tube itself
         vacuumMountMesh = trimesh.load(r"Mesh/SPARC_vacuum_mount.stl")
-        vacuumMount = vp.visuals.Mesh(vertices=vacuumMountMesh.vertices, faces=vacuumMountMesh.faces, color=(1,1,0.6,1))
+        vacuumMount = vs.visuals.Mesh(vertices=vacuumMountMesh.vertices, faces=vacuumMountMesh.faces, color=(1,1,0.6,1))
         vacuumMount.attach(vv.filters.mesh.ShadingFilter(shading="smooth"))
         self.view.add(vacuumMount)
 
         # Add copper coil anode
         anodeMesh = trimesh.load(r"Mesh/SPARC_vacuum_anode.stl")
-        anode = vp.visuals.Mesh(vertices=anodeMesh.vertices, faces=anodeMesh.faces, color=(0.7216, 0.451, 0.2, 1))
+        anode = vs.visuals.Mesh(vertices=anodeMesh.vertices, faces=anodeMesh.faces, color=(0.7216, 0.451, 0.2, 1))
         self.view.add(anode)
 
         # ****************** End of vacuum tube assembly *****************************
 
         # Add phosphor screen
         phosphorMesh = trimesh.load(r"Mesh/SPARC_phosphor.stl")
-        phosphorScreen = vp.visuals.Mesh(vertices=phosphorMesh.vertices, faces=phosphorMesh.faces, color=(0, 1, 0, 1))
+        phosphorScreen = vs.visuals.Mesh(vertices=phosphorMesh.vertices, faces=phosphorMesh.faces, color=(0, 1, 0, 1))
         phosphorScreen.attach(vv.filters.mesh.ShadingFilter(shading="smooth"))
         self.view.add(phosphorScreen)
 
@@ -120,7 +120,43 @@ class MainWindow(qt.QMainWindow):
         self.setSceneButton.setText("Load Scene")
         self.setSceneButton.setStyleSheet("border: 1px solid black;")
         self.inputLayout.layout().addWidget(self.setSceneButton,1,2)
-        
+
+    def addTrajectory(self, Voltage: float):
+        '''
+        addTrajectory adds a curve from the vacuum tube to the phosphor screen simulating the trajectory of the electron
+        beam. The curve will consist of 100 points. For the base configuraiton, this will just be a straight line.
+        For the extensions, this function will need to use an extrapolating algorithm to generate points sequentially
+        based on the equation of motion of the electrons. This function is a WIP and will preferably use a C++ library
+        in the future for added efficiency.
+        :param Voltage: voltage of the power source
+        :return: numpy 3x100 array
+        '''
+
+        # Determine line width based on voltage
+        V = 0
+        if Voltage < 10000:
+            V = 1
+        if 10000 < Voltage & Voltage < 50000:
+            V = 3
+        if 50000 < Voltage:
+            V = 5
+        # Declare numpy arrays for the x,y, and z coordinates of points along the curve
+        xcoords = np.zeros(100)
+        ycoords = np.linspace(-3.65, 4.775, 100)
+        zcoords = np.array([1.3 for x in range(0,100)])
+
+        # Declare uninitialized 3x100 array to store points on curve
+        curvePoints = np.empty(100,3)
+
+        # Add coordinates to curve
+        for i in range(0,100):
+            (curvePoints[i])[0] = xcoords[i]
+            (curvePoints[i])[1] = ycoords[i]
+            (curvePoints[i])[2] = zcoords[i]
+
+        # Create line in vispy and add it to scene canvas
+        curve = vs.Line(curvePoints, colo="blue", width=V)
+        self.view.add(curve)
 
 
 if __name__ == '__main__':

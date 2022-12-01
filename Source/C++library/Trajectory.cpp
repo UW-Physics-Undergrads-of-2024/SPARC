500ca1d2c156bbe35afa3f59dff804933e9322d4
= <kh4zhang@uwaterloo.ca>
2022-11-30 21:18:29 -0500

    Minor edits

--- Source/C++library/Trajectory.cpp
+++ Source/C++library/Trajectory.cpp
@@ -1,8 +1,7 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-#include <pybind11/common.h>
+#include "extern/pybind11/include/pybind11/pybind11.h"
+#include "extern/pybind11/include/pybind11/numpy.h"
+#include "extern/pybind11/include/pybind11/detail/common.h"
 #include "Trajectory.h"
-
 #include <functional>
 
 
@@ -29,10 +28,4 @@ py::array_t<double> classicalBeam(const int& voltage)
 
 // Hours wasted on PYBIND11_MODULE Bug: 6
 
-PYBIND11_MODULE(Trajectory, SPARC)
-{
-	SPARC.doc() = "A function that takes the voltage across the electrodes in the SPARC electron gun and produces a numpy array for the pointwise representation of the trajectory curve.";
-	SPARC.def("trajectoryClassical", &classicalBeam);
-}
-
-PYBIND11_NAMESPACE()
+PYBIND11_MODULE

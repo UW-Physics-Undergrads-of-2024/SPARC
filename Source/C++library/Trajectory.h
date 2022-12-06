4fb6c8b90351fa7d74d68a4e53830ea35e427f94
(Jack) Kuei Hung Zhang <kh4zhang@uwaterloo.ca>
2022-12-06 02:37:56 -0500

    Changed 'BFieldx' to 'b_field'

--- Source/C++library/Trajectory.h
+++ Source/C++library/Trajectory.h
@@ -8,7 +8,7 @@ const double mass_electron = 9.1093837015*std::pow(10, -31);
 const double charge_electron = -1.602176634 * std::pow(10, -19);
 constexpr double capacitor_gap = 0.10; // meters
 constexpr double delta = 0.001; // time interval when numerically determining position
-constexpr double BFieldx = 0.01; // magnetic flux density in the x-axis. Note that it is zero in the yz-plane
+constexpr double b_field = 0.01; // magnetic flux density in the x-axis. Note that it is zero in the yz-plane
 
 /**
 * @brief classicalBeam calculates the trajectory the electrons take and returns it as a set of 100 points

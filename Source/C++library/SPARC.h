a83be58e386df8cff038230cfaf80bc5d4f95e0a
= <kh4zhang@uwaterloo.ca>
2022-12-11 00:33:19 -0500

    Lowered value of delta

--- Source/C++library/SPARC.h
+++ Source/C++library/SPARC.h
@@ -6,8 +6,8 @@ namespace py = pybind11;
 
 const double mass_electron = 9.1093837015 * std::pow(10, -31);
 const double charge_electron = -1.602176634 * std::pow(10, -19);
-constexpr double capacitor_gap = 0.10; // meters
-constexpr double delta = 0.001; // time interval when numerically determining position
+constexpr double capacitor_gap = 0.50; // meters
+constexpr double delta = 0.00000000000001; // time interval when numerically determining position
 
 /**
 * @brief classicalBeam calculates the trajectory the electrons take and returns it as a set of 100 points
@@ -17,4 +17,4 @@ constexpr double delta = 0.001; // time interval when numerically determining po
 *
 * @return beam is the 100x3 numpy array representing points along the trajectory of the electron
 */
-std::vector<std::vector<double>> classicalBeam(const int& voltage);
+std::vector<std::vector<double>> classicalBeam(const double& voltage, const double& b_field);
